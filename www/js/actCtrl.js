5d060c7ddec0642c83639f3f4edf8753dafa4255
Robbie Fryers <Robbie@Robbies-MacBook-Pro.local>
2016-07-18 12:09:24 +0100

    All filters except category working

--- www/js/actCtrl.js
+++ www/js/actCtrl.js
@@ -2,33 +2,129 @@ var module = angular.module('maryhillControllers');
 
 module.controller('ActivityCtrl', function($scope, $http, $state, $ionicModal,ApiEndpoint, allInfo) {
 
-  $scope.modalData = [
-    {day: "Monday", checked: false},
-    {day: "Tuesday", checked: false},
-    {day: "Wednesday", checked: false},
-    {day: "Thursday", checked: false},
-    {day: "Friday", checked: false},
-    {day: "Saturday", checked: false},
-    {day: "Sunday", checked: false},
+	$scope.modalDays = [
+		{day: "Monday", checked: false},
+		{day: "Tuesday", checked: false},
+		{day: "Wednesday", checked: false},
+		{day: "Thursday", checked: false},
+		{day: "Friday", checked: false},
+		{day: "Saturday", checked: false},
+		{day: "Sunday", checked: false},
+	];
 
-];
+	$scope.agesUp = 65;
+	$scope.agesLow = 0;
+	$scope.startTime = 8;
+	$scope.endTime = 24;
 
-    $scope.print = function() {
-     console.log($scope.daysBox);
+	$scope.clearFilters = function() {
+		$scope.agesUp =65;
+		$scope.agesLow = 0;
+		$scope.startTime = 8;
+		$scope.endTime = 24;
+		for(i=0; i<7; i++){
+			$scope.modalDays[i].checked=false;
+		}
+		
+	}
 
-     console.log($scope.myData.days);
-    }
+	$scope.increaseStartTime = function() {
+		if($scope.startTime !=23) {
+			if($scope.startTime+1 == $scope.endTime){
+				$scope.endTime++;
+			}
+			$scope.startTime++;
+		}
+	};
+
+	$scope.decreaseStartTime = function() {
+		if($scope.startTime !=8) {
+			$scope.startTime--;
+		}
+	};
+
+	$scope.increaseEndTime = function() {
+		if($scope.endTime !=24) {
+			$scope.endTime++;
+		}
+	};
+
+	$scope.decreaseEndTime = function() {
+		if($scope.endTime !=9) {
+			if($scope.endTime-1 == $scope.startTime){
+				$scope.startTime--;
+			}
+			$scope.endTime--;
+		}
+	};
+
+
+	$scope.increaseLowAge = function() {
+		if($scope.agesLow !=60) {
+			if($scope.agesLow+5 == $scope.agesUp){
+				$scope.agesUp +=5;
+			}
+			$scope.agesLow +=5;
+		}
+	};
+
+	$scope.decreaseLowAge = function() {
+		if($scope.agesLow !=0) {
+			$scope.agesLow -=5;
+		}
+	};
+	$scope.increaseHighAge = function() {
+		if($scope.agesUp != 65){
+			$scope.agesUp += 5;
+		}
+	};
+
+	$scope.decreaseHighAge = function() {
+
+		if($scope.agesUp != 5){
+			if($scope.agesUp-5 == $scope.agesLow){		
+				$scope.agesLow -=5;
+			}
+			$scope.agesUp -= 5;
+		}
+	};	
+
+	$scope.searchTimes = function(item) {
+		for (i=0; i<item.days.length; i++){
+			if($scope.startTime <= parseInt(item.days[i].startTime.substring(0, 2)) && 
+				$scope.endTime >= parseInt(item.days[i].endTime.substring(0, 2))){
+				return true;
+			}
+
+		}	
+		return false;
+	}
+
+	$scope.searchAges = function(item) {
+		//case when max age is above max filtering age
+		if ($scope.agesUp == 65){
+			if($scope.agesLow <= item.agesUpper && ($scope.agesUp+34) >= item.agesLower) {
+				return true;
+			}
+		}
+		else{
+			if($scope.agesLow <= item.agesUpper && $scope.agesUp >= item.agesLower){
+				return true
+			}
+		}
+		return false;
+	}
 
     $scope.searchDays = function(item) {
     	//go through monday to friday check boxes
     	var count=0;
-    	for (i=0; i<$scope.modalData.length; i++) {
+    	for (i=0; i<$scope.modalDays.length; i++) {
     		//if a check box is checked, loop through items days to see if 
     		//they match the check box day
-    		if($scope.modalData[i].checked == true) {
+    		if($scope.modalDays[i].checked == true) {
     			count ++;
     			for (j=0; j<item.days.length; j++){
-    				if ((item.days[j].day.indexOf($scope.modalData[i].day)) !=-1){
+    				if ((item.days[j].day.indexOf($scope.modalDays[i].day)) !=-1){
     					return true; 
     				}
     			}
@@ -40,7 +136,7 @@ module.controller('ActivityCtrl', function($scope, $http, $state, $ionicModal,Ap
    			return true;
    		}
     	return false;
-    }
+    };
 
 
 
@@ -85,6 +181,7 @@ module.controller('ActivityCtrl', function($scope, $http, $state, $ionicModal,Ap
       url: ApiEndpoint.url + 'activities'
     }).then(function successCallback(response) {
       $scope.myData = response.data;
+      console.log($scope.myData);
       }, function errorCallback(response) {
 
       });
